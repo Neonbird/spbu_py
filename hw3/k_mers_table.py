6e97fd7fee8f4c3b1e058da718f0c39bcf3c0959
Tanya Maslikova <kosha44@yandex.ru>
2020-04-08 15:37:43 +0300

    done

--- hw3/k_mers_table.py
+++ hw3/k_mers_table.py
@@ -1,43 +1,69 @@
-from math import factorial
-from itertools import combinations_with_replacement
-
-# k = int(input())
-# seq = input()
-# m_size = factorial(3 + k) / (factorial(k) * 6)
-# table = [None for i in range(m_size)]
-
-def nucl_to_fourth(kmer):
-    nuc_to_num = {'A': 0, 'T': 1,
-                "G": 2, "C": 3}
-    num_to_nuc = {0: "A", 1: "T",
-                  2: "G", 3: "C"}
-    kmer = list(kmer)
+from sys import stdin
+from itertools import product
 
 
-def hash_fun(kmer: str, m_size: int):
-    hash = 0
-    i = 1
-    for nucl in reversed(kmer):
-        if nucl == 'A':
-            hash += i*1
-        elif nucl == 'C':
-            hash += i*2
-        elif nucl == 'G':
-            hash += i*3
-        else:
-            hash += i*4
-        i = i*10
-    return hash % m_size
-
-k = 4
-m_size = factorial(3 + k) / (factorial(k) * 6)
-kmers = combinations_with_replacement('ACGT', 4)
-d = []
-for i in kmers:
-    d.append(hash_fun(i, m_size))
-
-print(d)
+def kmer_to_fourth(kmer: str):
+    nuc_to_num = {'A': "0", 'T': "1",
+                  "G": "2", "C": "3"}
+    numeric_str = ""
+    for nucl in kmer:
+        numeric_str += (nuc_to_num[nucl])
+    return numeric_str
 
 
+def hash_for_kmer(number):
+    number = list(number)
+    number = [int(x) for x in number]
+    num_base_ten = 0
+    for index, numeral in enumerate(number[::-1]):
+        num_base_ten += int(numeral) * (4 ** index)
+    return num_base_ten
 
 
+def fourth_kmer_from_hash(hash_k, num_res_base=None):
+    if num_res_base is None:
+        num_res_base = []
+    remainder = hash_k % 4
+    quotient = hash_k // 4
+    num_res_base.append(remainder)
+    if quotient < 4:
+        num_res_base.append(quotient)
+    else:
+        fourth_kmer_from_hash(quotient, num_res_base)
+    return num_res_base[::-1]
+
+
+def make_hash_table(seq: str, k: int):
+    m = len(list(product(['A', 'C', 'G', 'T'], repeat=k)))
+    hash_table = [[0, None, None] for i in range(m)]
+    for i in range(len(seq)-k+1):
+        kmer = seq[i:i+k]
+        kmer_fourth = kmer_to_fourth(kmer)
+        kmer_hash = hash_for_kmer(kmer_fourth)
+        hash_table[kmer_hash][0] += 1
+        if not hash_table[kmer_hash][1] or hash_table[kmer_hash][1] > i:
+            hash_table[kmer_hash][1] = i
+        if not hash_table[kmer_hash][2] or hash_table[kmer_hash][2] < i:
+            hash_table[kmer_hash][2] = i
+    return hash_table
+
+
+def print_kmers_table(hash_table, k):
+    for i_line in range(len(hash_table)):
+        if hash_table[i_line][0] != 0:
+            fourth_kmer = ''.join([str(i) for i in fourth_kmer_from_hash(i_line)])
+            if len(fourth_kmer) < k:
+                fourth_kmer = "A"*(k - len(fourth_kmer)) + fourth_kmer
+            kmer = fourth_kmer.translate(str.maketrans({'0': 'A', '1': 'T', '2': 'G', '3': 'C'}))
+            reversed(kmer)
+            print('{}\t{}\t{}\t{}'.format(kmer, hash_table[i_line][0], hash_table[i_line][1], hash_table[i_line][2]))
+
+
+if __name__ == "__main__":
+    k = int(input("k value: "))
+    for path_to_file in stdin:
+        with open(path_to_file.strip()) as file:
+            name = file.readline().strip()
+            seq = file.read().replace('\n', '')
+        hash_table = make_hash_table(seq, k)
+        print_kmers_table(hash_table, k)
